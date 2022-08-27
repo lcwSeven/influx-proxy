a42cdae7c3e5984883b08db07c16be3ca8394036
liucaiwenseven@163.com <liucaiwenseven@163.com>
2022-08-27 19:41:43 +0800

    init

---
+++ src/main/java/com/influx/proxy/InfluxProxyApplication.java
@@ -0,0 +1,16 @@
+package com.influx.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liucaiwen
+ * @date 2022/08/27 19:36
+ */
+@SpringBootApplication
+public class InfluxProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluxProxyApplication.class, args);
+    }
+}
